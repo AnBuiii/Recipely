cb021d959f6d119da10b2bba3338ed08a4da8443
Bùi Lê Hoài An <builehoaian2002@gmail.com>
2023-07-19 11:19:29 +0700

    Implemented ForgotPasswordScreen

---
+++ app/src/main/java/com/anbui/recipely/presentation/create_account/CreateAccountScreen.kt
@@ -0,0 +1,11 @@
+package com.anbui.recipely.presentation.create_account
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CreateAccountScreen(
+    navController: NavController
+) {
+
+}
