c80bb14ebf33e16b2a6b3a6526b8a31c0dd5a0a4
Bùi Lê Hoài An <builehoaian2002@gmail.com>
2023-09-06 12:16:06 +0700

    Implementing OrderDetail

--- app/src/main/java/com/anbui/recipely/presentation/components/Stepper.kt
+++ app/src/main/java/com/anbui/recipely/presentation/components/Stepper.kt
@@ -7,11 +7,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,6 +27,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
@@ -34,24 +36,28 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.anbui.recipely.R
+import com.anbui.recipely.presentation.ui.theme.DarkGrey
 import com.anbui.recipely.presentation.ui.theme.SpaceSmall
+import com.anbui.recipely.presentation.ui.theme.TrueWhite
 import kotlin.math.roundToInt
 
 @Composable
-fun Stepper(modifier: Modifier = Modifier,
-            numberOfSteps: Int,
-            currentStep: Int,
-            stepDescriptionList : List<String> = List(numberOfSteps){""},
-            unSelectedColor:Color = Color.LightGray,
-            selectedColor:Color? = null,
-            isRainbow:Boolean = false
+fun Stepper(
+    modifier: Modifier = Modifier,
+    numberOfSteps: Int,
+    currentStep: Int,
+    stepDescriptionList: List<String> = List(numberOfSteps) { "" },
+    unSelectedColor: Color = Color.LightGray,
+    selectedColor: Color? = null,
+    isRainbow: Boolean = false
 ) {
 
-    val descriptionList= MutableList(numberOfSteps){""}
+    val descriptionList = MutableList(numberOfSteps) { "" }
 
     stepDescriptionList.forEachIndexed { index, element ->
-        if (index<numberOfSteps)
-            descriptionList[index]=element
+        if (index < numberOfSteps)
+            descriptionList[index] = element
     }
 
     Column(
@@ -60,14 +66,15 @@ fun Stepper(modifier: Modifier = Modifier,
     ) {
         for (step in 1..numberOfSteps) {
             Step(
-                modifier = Modifier.weight(1F),
+                modifier = if (step != numberOfSteps) Modifier.height(80.dp) else Modifier,
+//                modifier =  Modifier.height(60.dp),
                 step = step,
                 isCompete = step < currentStep,
                 isCurrent = step == currentStep,
-                isComplete= step == numberOfSteps,
+                isComplete = step == numberOfSteps,
                 isRainbow = isRainbow,
-                stepDescription= descriptionList[step-1],
-                unSelectedColor=unSelectedColor,
+                stepDescription = descriptionList[step - 1],
+                unSelectedColor = unSelectedColor,
                 selectedColor = selectedColor,
             )
         }
@@ -75,15 +82,16 @@ fun Stepper(modifier: Modifier = Modifier,
 }
 
 @Composable
-private fun Step(modifier: Modifier = Modifier,
-                 step :Int,
-                 isCompete: Boolean,
-                 isCurrent: Boolean,
-                 isComplete:Boolean,
-                 isRainbow: Boolean,
-                 stepDescription:String,
-                 unSelectedColor:Color,
-                 selectedColor: Color?,
+private fun Step(
+    modifier: Modifier = Modifier,
+    step: Int,
+    isCompete: Boolean,
+    isCurrent: Boolean,
+    isComplete: Boolean,
+    isRainbow: Boolean,
+    stepDescription: String,
+    unSelectedColor: Color,
+    selectedColor: Color?,
 ) {
 
     val rainBowColor = Brush.linearGradient(
@@ -100,17 +108,17 @@ private fun Step(modifier: Modifier = Modifier,
     val transition = updateTransition(isCompete, label = "")
 
     val innerCircleColor by transition.animateColor(label = "innerCircleColor") {
-        if (it) selectedColor?: MaterialTheme.colorScheme.primary else unSelectedColor
+        if (it) selectedColor ?: MaterialTheme.colorScheme.primary else unSelectedColor
     }
     val txtColor by transition.animateColor(label = "txtColor")
-    { if (it || isCurrent) selectedColor?:MaterialTheme.colorScheme.primary else unSelectedColor }
+    { if (it || isCurrent) selectedColor ?: MaterialTheme.colorScheme.primary else unSelectedColor }
 
     val color by transition.animateColor(label = "color")
-    { if (it || isCurrent) selectedColor?:MaterialTheme.colorScheme.primary else Color.Gray }
+    { if (it || isCurrent) selectedColor ?: MaterialTheme.colorScheme.primary else Color.Gray }
 
-    val borderStroke:BorderStroke = if (isRainbow){
+    val borderStroke: BorderStroke = if (isRainbow) {
         BorderStroke(2.dp, rainBowColor)
-    }else{
+    } else {
         BorderStroke(2.dp, color)
     }
 
@@ -120,58 +128,71 @@ private fun Step(modifier: Modifier = Modifier,
 
         val (circle, txt, line) = createRefs()
 
-        Surface(
-            shape = CircleShape,
-            border = borderStroke,
-            color = unSelectedColor,
-            modifier = Modifier.size(30.dp).constrainAs(circle) {
-                top.linkTo(parent.top)
-                bottom.linkTo(line.top)
-                start.linkTo(parent.start)
-            }
-        ) {
-
-            Box(contentAlignment = Alignment.Center) {
-                Text(
-                    text = step.toString(),
-                    color =  Color.Gray,
-                    fontSize = 9.sp)
-            }
-        }
-
-        Text(
+        Icon(
+            painter = painterResource(id = R.drawable.ic_time),
+            contentDescription = "",
+            modifier = Modifier
+                .constrainAs(circle) {
+                    top.linkTo(parent.top, if (isComplete) -12.dp else 0.dp)
+                    bottom.linkTo(line.top)
+                    start.linkTo(parent.start)
+                }
+                .size(24.dp)
+                .background(TrueWhite),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+        Column(
             modifier = Modifier.constrainAs(txt) {
                 top.linkTo(circle.top)
                 start.linkTo(circle.end, margin = SpaceSmall)
                 end.linkTo(parent.end)
                 bottom.linkTo(circle.bottom)
-            },
-            fontSize = textSize,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            text = stepDescription,
-            color  = txtColor,
-        )
+            }
+        ) {
+            Text(
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                text = stepDescription,
+                style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Medium)
+            )
+            Text(
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                text = "20:09, Today",
+                style = MaterialTheme.typography.bodySmall.copy(
+                    fontWeight = FontWeight.Normal,
+                    color = DarkGrey
+                )
+            )
+        }
+
+
 
         if (!isComplete) {
             Box(
                 modifier = Modifier
-                    .fillMaxHeight()  //fill the max height
-                    .width(1.dp).constrainAs(line){
-                    top.linkTo(circle.bottom)
-                    start.linkTo(circle.start)
-                    end.linkTo(circle.end)
-                    bottom.linkTo(parent.bottom)
-                }
+//                    .height(30.dp)  //fill the max height
+                    .fillMaxHeight()
+                    .width(1.dp)
+                    .constrainAs(line) {
+                        top.linkTo(circle.bottom)
+                        start.linkTo(circle.start)
+                        end.linkTo(circle.end)
+                        bottom.linkTo(parent.bottom)
+                    }
 
 
-                    .background(Color.Black, shape = VerticalDottedShape(step = 10.dp)),
+                    .background(
+                        MaterialTheme.colorScheme.secondary,
+                        shape = VerticalDottedShape(step = 10.dp)
+                    ),
 //                color = innerCircleColor,
 //                thickness = 1.dp,
             )
         }
     }
 }
+
 private data class VerticalDottedShape(
     val step: Dp,
 ) : Shape {
@@ -183,11 +204,11 @@ private data class VerticalDottedShape(
         val stepPx = with(density) { step.toPx() }
         val stepsCount = (size.height / stepPx).roundToInt()
         val actualStep = size.height / stepsCount
-        val dotSize = Size(width = size.width,height = actualStep / 2 )
+        val dotSize = Size(width = size.width, height = actualStep / 2)
         for (i in 0 until stepsCount) {
             addRect(
                 Rect(
-                    offset = Offset( x = 0f,y = i * actualStep),
+                    offset = Offset(x = 0f, y = i * actualStep),
                     size = dotSize
                 )
             )
--- app/src/main/java/com/anbui/recipely/presentation/order_detail/OrderDetailScreen.kt
+++ app/src/main/java/com/anbui/recipely/presentation/order_detail/OrderDetailScreen.kt
@@ -1,17 +1,24 @@
 package com.anbui.recipely.presentation.order_detail
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.anbui.recipely.R
+import com.anbui.recipely.presentation.components.StandardCard
 import com.anbui.recipely.presentation.components.StandardToolbar
 import com.anbui.recipely.presentation.components.Stepper
+import com.anbui.recipely.presentation.ui.theme.SpaceLarge
 
 @ExperimentalMaterial3Api
 @Composable
@@ -21,17 +28,37 @@ fun OrderDetailScreen(
     Column {
         StandardToolbar(
             navController = navController,
-            title = "Order #3123123",
+            title = "Order",
             showBackArrow = true
         )
         val numberStep = 4
-        var currentStep by rememberSaveable { mutableStateOf(4) }
-        val titleList= arrayListOf("Step 1","Step 2","Step 3","Step 4")
+        var currentStep by rememberSaveable { mutableStateOf(1) }
+        val titleList = arrayListOf("Step 1", "Step 2", "Step 3", "Step 4")
 
-        Stepper(
-            numberOfSteps = numberStep,
-            currentStep = currentStep,
-            stepDescriptionList = titleList
-        )
+        StandardCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = SpaceLarge),
+        ) {
+            Text(text = "Order ID: 123321")
+            Text(text = "Order date: 20:05, Yesterday")
+            Text(text = "Success delivery")
+        }
+        StandardCard(
+            modifier = Modifier
+                .fillMaxWidth()
+//                .height(300.dp)
+                .padding(horizontal = SpaceLarge),
+        ) {
+            Text(text = "Order status")
+            Spacer(modifier = Modifier.height(SpaceLarge))
+            Stepper(
+                numberOfSteps = numberStep,
+                currentStep = currentStep,
+                stepDescriptionList = titleList,
+            )
+        }
+
+
     }
 }
